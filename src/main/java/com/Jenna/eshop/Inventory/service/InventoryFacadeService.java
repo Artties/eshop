0454793f0fdf9d0c7ea5a6e383b50cdae1c85b3a
Jenna He <hecncn@126.com>
2023-03-16 17:33:02 +0800

    更新公用API组件

--- src/main/java/com/Jenna/eshop/Inventory/service/InventoryFacadeService.java
+++ src/main/java/com/Jenna/eshop/Inventory/service/InventoryFacadeService.java
@@ -1,11 +1,13 @@
 package com.Jenna.eshop.Inventory.service;
 
-import com.Jenna.eshop.wms.dto.PurchaseInputOrderDTO;
-import com.Jenna.eshop.order.dto.OrderDTO;
-import com.Jenna.eshop.wms.dto.ReturnGoodsInputOrderDTO;
+import com.Jenna.eshop.wms.domain.PurchaseInputOrderDTO;
+import com.Jenna.eshop.order.domain.OrderInfoDTO;
+import com.Jenna.eshop.wms.domain.ReturnGoodsInputOrderDTO;
 
 /**
  * 库存中心对外提供接口
+ * @author Jenna C He
+ * @date 2023/03/15/ 09:15
  */
 public interface InventoryFacadeService {
     /**
@@ -17,24 +19,24 @@ public interface InventoryFacadeService {
 
     /**
      * 通知库存中心，"提交订单"事件发生了
-     * @param orderDTO 订单DTO
+     * @param orderInfoDTO 订单DTO
      * @return 处理结果
      */
-    Boolean informSubmitOrderEvent(OrderDTO orderDTO);
+    Boolean informSubmitOrderEvent(OrderInfoDTO orderInfoDTO);
 
     /**
      * 通知库存中心，"支付订单"事件发生了
-     * @param orderDTO 订单DTO
+     * @param orderInfoDTO 订单DTO
      * @return 处理结果
      */
-    Boolean informPayOrderEvent(OrderDTO orderDTO);
+    Boolean informPayOrderEvent(OrderInfoDTO orderInfoDTO);
 
     /**
      * 通知库存中心，"取消订单"事件发生了
-     * @param orderDTO 订单DTO
+     * @param orderInfoDTO 订单DTO
      * @return 处理结果
      */
-    Boolean informCancelOrderEvent(OrderDTO orderDTO);
+    Boolean informCancelOrderEvent(OrderInfoDTO orderInfoDTO);
 
     /**
      * 通知库存中心，"完成退货入库"事件发生了
