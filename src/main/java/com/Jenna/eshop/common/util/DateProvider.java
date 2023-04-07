41bea385f412d9b065d1c6e38fe3fd3209799812
Jenna He <hecncn@126.com>
2023-04-07 15:43:54 +0800

    更新组件

--- src/main/java/com/Jenna/eshop/common/util/DateProvider.java
+++ src/main/java/com/Jenna/eshop/common/util/DateProvider.java
@@ -7,13 +7,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * 日期辅助组件
+ * 日期辅助组件接口
  * @author Jenna C He
  *
  */
 @Component
 public interface DateProvider {
-
+    /**
+     * 获取当前时间
+     * @return 当前时间
+     * @throws Exception 抛出异常
+     */
     public Date getCurrentTime() throws Exception;
 
 }
