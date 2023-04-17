affa688bb6cc60acaa2d4cabbef6929534a4e142
Jenna He <hecncn@126.com>
2023-04-17 11:12:30 +0800

    更新文件排序

--- src/main/java/test/java/com/Jenna/eshop/auth/dao/AccountPriorityRelationshipDAOTest.java
+++ src/main/java/test/java/com/Jenna/eshop/auth/dao/AccountPriorityRelationshipDAOTest.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.Optional;
 
 /**
  * 账号和权限关系管理模块的DAO组件的单元测试类
