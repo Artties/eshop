b1684d192e5d579891e308f56f8e6895922f87f8
Jenna He <hecncn@126.com>
2023-04-20 10:59:39 +0800

    更新D分支

---
+++ src/main/java/com/Jenna/eshop/Inventory/service/InventoryFacadeService.java
@@ -0,0 +1,54 @@
+package com.Jenna.eshop.Inventory.service;
+
+import com.Jenna.eshop.wms.domain.PurchaseInputOrderDTO;
+import com.Jenna.eshop.order.domain.OrderInfoDTO;
+import com.Jenna.eshop.wms.domain.ReturnGoodsInputOrderDTO;
+
+/**
+ * 库存中心对外提供接口
+ * @author Jenna C He
+ * @date 2023/03/15/ 09:15
+ */
+public interface InventoryFacadeService {
+    /**
+     * 通知库存中心，"采购入库完成"事件发生了
+     * @param purchaseInputOrderDTO 采购入库单DTO
+     * @return 处理结果
+     */
+    Boolean informPurchaseInputFinished(PurchaseInputOrderDTO purchaseInputOrderDTO);
+
+    /**
+     * 通知库存中心，"提交订单"事件发生了
+     * @param orderInfoDTO 订单DTO
+     * @return 处理结果
+     */
+    Boolean informSubmitOrderEvent(OrderInfoDTO orderInfoDTO);
+
+    /**
+     * 通知库存中心，"支付订单"事件发生了
+     * @param orderInfoDTO 订单DTO
+     * @return 处理结果
+     */
+    Boolean informPayOrderEvent(OrderInfoDTO orderInfoDTO);
+
+    /**
+     * 通知库存中心，"取消订单"事件发生了
+     * @param orderInfoDTO 订单DTO
+     * @return 处理结果
+     */
+    Boolean informCancelOrderEvent(OrderInfoDTO orderInfoDTO);
+
+    /**
+     * 通知库存中心，"完成退货入库"事件发生了
+     * @param returnGoodsInputOrderDTO 退货入库单DTO
+     * @return 处理结果
+     */
+    Boolean informReturnGoodsInputFinished(ReturnGoodsInputOrderDTO returnGoodsInputOrderDTO);
+
+    /**
+     * 查询商品sku的库存
+     * @param goodsSkuId 商品sku id
+     * @return 商品sku的库存
+     */
+    Long getSaleStockQuantity(Long goodsSkuId);
+}
