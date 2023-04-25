48352b12a4dab1f5e13d0b00d53a87f695a8c478
Jenna He <hecncn@126.com>
2023-04-25 11:47:56 +0800

    在common api中增加三个工具类

---
+++ src/main/java/com/Jenna/eshop/common/util/DomainType.java
@@ -0,0 +1,25 @@
+package com.Jenna.eshop.common.util;
+
+/**
+ * @author Jenna C He
+ * @date 2023/04/25 11:46
+ */
+public class DomainType {
+
+    /**
+     * VO：Value Object
+     */
+    public static final String VO = "VO";
+    /**
+     * DTO：Data Transfer Object
+     */
+    public static final String DTO = "DTO";
+    /**
+     * DO：Data Object
+     */
+    public static final String DO = "DO";
+
+    private DomainType() {
+
+    }
+}
