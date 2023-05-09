59b14a46f9e14813efaad79409cd7a228a104463
Jenna He <hecncn@126.com>
2023-05-09 20:10:01 +0800

    更新代码

---
+++ src/main/java/com/Jenna/eshop/comment/service/CommentInfoService 2.java
@@ -0,0 +1,29 @@
+package com.Jenna.eshop.comment.service;
+
+import com.Jenna.eshop.comment.domain.CommentInfoDTO;
+import com.Jenna.eshop.order.domain.OrderInfoDTO;
+import com.Jenna.eshop.order.domain.OrderItemDTO;
+
+/**
+ * 评论信息管理模块service组件接口
+ * @author Jenna C He
+ * @date 2023/1/6 13:09
+ */
+public interface CommentInfoService {
+
+    /**
+     * 新增手动发表评论信息
+     * @param commentInfoDTO 评论信息DTO对象
+     * @return 是否保存评论信息
+     */
+    Boolean saveManualPublishedCommentInfo(CommentInfoDTO commentInfoDTO);
+
+    /**
+     * 新增自动发表的评论信息
+     * @param orderInfoDTO 订单信息DTO对象
+     * @param orderItemDTO 订单条目DTO对象
+     * @return 处理结果
+     */
+
+    CommentInfoDTO saveAutoPublishedCommentInfo(OrderInfoDTO orderInfoDTO, OrderItemDTO orderItemDTO);
+}
